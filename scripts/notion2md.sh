b3225353de2330e5d23af9b08987584f769559b3
tomoki_sun <tomoki_69386@yahoo.co.jp>
2023-03-04 15:19:11 +0900

    Update scripts

--- scripts/notion2md.sh
+++ scripts/notion2md.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yarn run notion2md --id ded5b98626ea4291816a629126ff6369 -f README.md
-yarn run notion2md --id 080759143c4e44a1ad34df5e2fa8d351 -f Accounts.md
+yarn run notion2md --id 080759143c4e44a1ad34df5e2fa8d351 -f accounts.md
