e63b8ffa0c9460644fb362990ae272945707b621
tomoki_sun <tomoki_69386@yahoo.co.jp>
2023-03-04 15:34:28 +0900

    Create what-is-this.md

--- scripts/notion2md.sh
+++ scripts/notion2md.sh
@@ -2,3 +2,4 @@
 
 yarn run notion2md --id ded5b98626ea4291816a629126ff6369 -f README.md
 yarn run notion2md --id 080759143c4e44a1ad34df5e2fa8d351 -f accounts.md
+yarn run notion2md --id 2651035eaba54d83b731622394cce5ae -f what-is-this.md
